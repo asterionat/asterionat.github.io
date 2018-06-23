9e043b6e8c7daab2c61e9f39e7b91666d2cd94cf
JeronimoCarranza <jeronimo.carranza@asterionat.com>
2018-06-23 17:45:33 +0200

    bilingual version ES, EN

---
+++ js/locale.js
@@ -0,0 +1,131 @@
+/* 
+ * Locale implementation.
+ * 
+ * @author Jerónimo Carranza Carranza
+ */
+
+localeES={
+	language: 'ES'
+	,services:'SERVICIOS'
+	,technologies:'TECNOLOGÍAS'
+	,team: 'EQUIPO'
+	,projects: 'PROYECTOS'
+	,contact: 'CONTACTO'
+	,intro: 'Datos, Mapas & Medio Ambiente'
+	,btnLang: 'ES->EN'
+	,aboutText: 'ASTERIONAT es una empresa consultora creada en 2016 con el objetivo de proveer servicios especializados de alto valor añadido en ciencia de datos, información territorial y medio ambiente a empresas, administraciones públicas y otras organizaciones.<br><br> En ASTERIONAT estamos especializados en sacar el máximo partido a los datos y la información geográfica, en particular a datos e información ambiental.'
+	,servicesHead: 'Servicios'
+	,servicesAreas: 'ASTERIONAT ofrece servicios de <b>Consultoría, Asistencia Técnica, Desarrollo, Implantación, Mantenimiento y Formación</b> en sus tres áreas de competencia.'
+	,servicesStatisticsHead: 'Estadística y Ciencia de los Datos'
+	,servicesStatistics: 'Planificación estadística, diseños muestrales, muestreo y encuestación, análisis estadístico, aprendizaje estadístico, aprendizaje automático, aprendizaje profundo, modelos explicativos, modelos predictivos, explotación de sistemas, inteligencia de negocio, consultoría estratégica y tecnológica, implantación de soluciones ERP, BI, Big Data, personalización y desarrollos específicos, anonimización y securización de datos, auditoría LOPD y LSSI, asesoría, formación, ...' 
+	,servicesGISHead: 'Información Territorial y Sistemas de Información Geográfica'
+	,servicesGIS: 'Planificación, gestión y captura de datos espaciales, teledetección, producción cartográfica, modelos digitales del terreno, diseño y explotación de geodatabases, análisis y modelado geo-espacial, desarrollo de sistemas GIS, WebGIS, implantación y mantenimiento de infraestructuras de datos espaciales (OGC, INSPIRE), ...'
+	,servicesEnvironmentHead: 'Ecología y Medio Ambiente'
+	,servicesEnvironment: 'Estudios ecológicos, ambientales y territoriales, planificación hidrológica, planificación y gestión de espacios naturales, estadísticas ambientales, modelización y simulación ambiental, estudios de impacto ambiental, proyectos de restauración ambiental, seguimiento y vigilancia ambiental, estudios de huella ecológica, huella hídrica, huella de carbono, balance de CO2, auditoría ambiental, ...'
+	,technologiesHead: 'Tecnologías'
+	,technologiesText: 'Conocimiento y Tecnología son dos pilares básicos sobre los que se sustenta nuestra actividad. ASTERIONAT basa sus servicios fundamentalmente en <b>Estándares y Tecnologías Abiertas, Software Libre</b> y, allí donde es posible, en <b>Datos Abiertos</b>.'
+	,teamHead: 'Equipo'
+	,teamText: 'ASTERIONAT cuenta con un equipo de profesionales <b>multidisciplinar y altamente cualificado</b> que opera bajo la coordinación de su fundador y director.'
+	,JCCText: 'Fundador y Director General<br><i>Científico de Datos, Estadístico y Biólogo</i>'
+	,projectsHead: 'Proyectos'
+	,projectsText: 'ASTERIONAT es una empresa muy joven que cuenta sin embargo con la dilatada experiencia, de más de 25 años, de su equipo de profesionales. <br>Aquí se señalan algunas referencias de su trabajo.'
+	,saneaclm: 'SaneaCLM'
+	,saneaclmTitle: '<br>Sistema de Información de Saneamiento y Depuración de Castilla - La Mancha'
+	,rustic3a: 'Rustic3a'
+	,rustic3aTitle: '<br>Sistema de Información Geográfica de las Parcelas Rústicas de titularidad de la Agencia Andaluza del Agua en Huelva'
+	,qnatgv: 'Caudales Naturales Guadalquivir'
+	,qnatgvTitle: '<br>Sínstesis estadística del régimen natural de caudales (1994 - 2006) en la Cuenca del Guadalquivir'
+	,gvccapiloto: 'Guadalquivir Cuenca Piloto DMA-Agricultura'
+	,gvccapilotoTitle: '<br>Trabajos de la Confederación Hidrográfica del Guadalquivir como cuenca piloto europea en la segunda fase de implantación de la Directiva Marco de Aguas'
+	,brocal: 'Brocal'
+	,brocalTitle: '<br>Sistema de Información Geográfica de la Calidad de las Aguas Subterráneas de la Demarcación Hidrográfica del Guadalquivir'
+	,bdsociohidrogv: 'BBDD Socio-Hidrológica <br>CH Guadiana'
+	,bdsociohidrogvTitle: '<br>Actualización e Integración de Bases de Datos de Información Socio-Hidrológica de la Confederación Hidrográfica del Guadiana'
+	,contactHead: 'Contacto'
+	,legallink: 'Aviso legal'
+	,privlink: 'Política de privacidad'
+	,legalTitle: 'Aviso Legal'
+	,privTitle: 'Política de Privacidad'
+	,btnCloseLegal: 'Cerrar'
+	,btnClosePriv: 'Cerrar'
+	,cookiesText: 'Este sitio web utiliza cookies propias y de terceros, si continúa navegando consideramos que acepta su uso.'
+}
+
+localeEN={
+	language: 'EN'
+	,services:'SERVICES'
+	,technologies:'TECHNOLOGIES'
+	,team: 'TEAM'
+	,projects: 'PROJECTS'
+	,contact: 'CONTACT'
+	,intro: 'Data, Maps & Environment'
+	,btnLang: 'EN->ES'
+	,aboutText: 'ASTERIONAT is a consulting company created in 2016 with the aim of providing specialized services of high added value in data science, territorial information and environment to companies, public administrations and other organizations. <br> <br> In ASTERIONAT we are specialized in taking out the maximum match to data and, geographic information, in particular to environmental data and information.'
+	,servicesHead: 'Services'
+	,servicesAreas: 'ASTERIONAT offers <b>Consulting, Technical Assistance, Development, Implementation, Maintenance and Training</b> services in its three areas of competence.'
+	,servicesStatisticsHead: 'Statistics & Data Science'
+	,servicesStatistics: 'Statistical planning, sample designs, sampling and survey, statistical analysis, statistical learning, machine learning, deep learning, explanatory models, predictive models, systems exploitation, business intelligence, strategic and technological consulting, solutions implementation of ERP, BI, Big Data, personalization and specific developments, anonymization and data security, LOPD and LSSI audit, advice, training, ...' 
+	,servicesGISHead: 'Geospatial Information & Geographic Information Systems'
+	,servicesGIS: 'Planning, management and capture of spatial data, remote sensing, cartographic production, digital terrain models, design and exploitation of geodatabases, analysis and geo-spatial modeling, development of GIS, WebGIS, implementation and maintenance of Spatial Data Infrastructures (OGC, INSPIRE), ...'
+	,servicesEnvironmentHead: 'Ecology & Environment'
+	,servicesEnvironment: 'Ecological, environmental and territorial studies, hydrological planning, planning and management of natural areas, environmental statistics, modeling and environmental simulation, environmental impact studies, environmental restoration projects, environmental monitoring, ecological footprint studies, water footprint, carbon footprint, CO2 balance, environmental audit, ...'
+	,technologiesHead: 'Technologies'
+	,technologiesText: "Knowledge and Technology are two basic pillars on which our activity is based. ASTERIONAT bases its services mainly on <b>Open Standards and Technologies, Open Source Software</b> and, where's possible, on <b>Open Data</b>."
+	,teamHead: 'Team'
+	,teamText: 'ASTERIONAT has a team of <b> multidisciplinary and highly qualified professionals </b> that operates under the coordination of its founder and director.'
+	,JCCText: 'Founder & CEO<br><i>Data Scientist, Statistician & Biologist</i>'
+	,projectsHead: 'Projects'
+	,projectsText: 'ASTERIONAT is a very young company that has, however, the extensive experience of more than 25 years of its team of professionals. <br> Here are some references of his work.'
+	,saneaclm: 'SaneaCLM'
+	,saneaclmTitle: '<br>Information System of Sanitation of Urban Wastewater Treatment in Castilla–La Mancha.'
+	,rustic3a: 'Rustic3a'
+	,rustic3aTitle: '<br>Geographic Information System of the rural land plots of Andalusian Water Agency in Huelva.'
+	,qnatgv: 'Natural Flows of Guadalquivir Basin'
+	,qnatgvTitle: '<br>Statistical summary of natural flow in the Guadalquivir river basin in the period 1940 – 2006 and allocation to hydrographical network.'
+	,gvccapiloto: 'Guadalquivir Pilot River Basin for WFD-Agriculture'
+	,gvccapilotoTitle: '<br>Technical Assistance to the Guadalquivir Basin Authority as European Pilot River Basin in the second phase of implementation of the Water Framework Directive.'
+	,brocal: 'Brocal'
+	,brocalTitle: '<br>Geographic Information System of Groundwater Quality in the Guadalquivir Basin.'
+	,bdsociohidrogv: 'Socio-Hydrologic Databases<br> in Guadiana Basin'
+	,bdsociohidrogvTitle: '<br>Technical Assistance to the Guadiana Basin Authority for the update and integration of socio-hydrological information databases.'
+	,contactHead: 'Contact'
+	,legallink: 'Legal notice'
+	,privlink: 'Privacy policy'
+	,legalTitle: 'Legal Notice in Spanish'
+	,privTitle: 'Privacy Policy in Spanish'
+	,btnCloseLegal: 'Close'
+	,btnClosePriv: 'Close'
+	,cookiesText: 'This website uses its own and third-party cookies, if you continue browsing we consider that you accept its use.'
+}
+
+initialLanguage();
+
+
+function initialLanguage(){
+	if(navigator.language.toUpperCase().includes()=='ES') locale = localeES;
+	else locale = localeEN;
+}
+
+function changeLanguage(lang){
+	if(lang=='ES') locale = localeES;
+	if(lang=='EN') locale = localeEN;
+}
+
+function applyLanguage(){
+	var elements = document.getElementsByTagName("*");
+	//console.log(elements.length);
+	for (i = 0; i < elements.length; i++){
+		if (elements[i].id.includes("locale.")) {
+			elements[i].innerHTML = eval(elements[i].id.valueOf());
+			//console.log(i,'-',elements[i].id,'-',elements[i].innerHTML);
+		}  
+	}
+}
+
+function clickLanguage(){
+	if(locale.language=='ES') this.changeLanguage('EN');
+	else this.changeLanguage('ES');
+	this.applyLanguage();
+	//alert('Cambio a: '+locale.language);
+}
+
